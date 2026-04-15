7026b1163784186b8b7f7b2b0bacbecf079aab72
hafssa01 <hafssazaferr@gmail.com>
2026-04-15 20:55:44 +0100

    adding Exercice2.java

---
+++ java/td-tp02/Exercice2.java
@@ -0,0 +1,8 @@
+public class Exercice2{
+	public static void main(String[] args){
+		String phrase = new String("le petit elephant gris");
+		String resultat = phrase.replace('e', 'u').replace('i','u');
+		System.out.println(resultat);
+
+	}
+}
