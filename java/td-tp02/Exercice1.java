95cacb0f1ca27f8819c5ecec72dbc7658b01b067
hafssa01 <hafssazaferr@gmail.com>
2026-04-15 20:30:05 +0100

    adding Exercice1.java with 2 directories

---
+++ java/td-tp02/Exercice1.java
@@ -0,0 +1,8 @@
+class Exercice1{
+	public static void main(String[] args){
+		String original = new String("exemple de texte");
+		String result = original.replace('e','u');
+		System.out.println(result);
+	}
+
+}
