49645de04b4f3b833a93ecfe93a5653bfe683ec2
hafssa01 <hafssazaferr@gmail.com>
2026-04-15 22:04:10 +0100

    Exercice3.java -> done

---
+++ java/td-tp02/Exercice3.java
@@ -0,0 +1,10 @@
+public class Exercice3 {
+    public static void main(String[] args){
+        String phrase = new String("Ceci est une phrase.");
+        if(phrase.endsWith(".")){
+            System.out.println("La chaine se termine par un point.");
+        } else {
+            System.out.println("Pas de point a la fin.");
+        }
+    }
+}
